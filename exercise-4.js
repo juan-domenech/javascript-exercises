07549809ca09401441f41ed45756532dd4d311e3
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-11 13:03:00 +0000

    javascript-exercises 4 multiple columns

--- exercise-4.js
+++ exercise-4.js
@@ -1,11 +1,21 @@
 function multiply(){
+
 	var numElements = arguments.length;
-	console.log("Number of arguments:",numElements);
-	for (i=0; i<numElements; i++){
-		var valueElement = arguments[i];
-		console.log("value:",valueElement)
-		for (y=1; y<=10; y++){
-			console.log(valueElement+" x "+y+" = "+valueElement*y);
+	//console.log("Number of arguments:",numElements);
+
+	//Loop the 10 rows of the multiplication table
+	for (x=1; x<=10; x++){
+		var result="";
+		//Loop as many arguments
+		for (y=0; y<numElements; y++){
+
+				//Get the current argument
+				var valueElement = arguments[y];
+				result = result+valueElement+" x "+x+" = "+x*valueElement+"\t";
 		}
+		//Print row
+		console.log(result);
+		result="";
 	}
 }
+
