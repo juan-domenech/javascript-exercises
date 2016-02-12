24cc46b570c548a83a8837d06e1dcf3bde28691a
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-12 12:32:32 +0000

    javascript-exercises: 02-1 with indexOf by me :)

---
+++ 02-more-steps/exercise-1-with-indexOf.js
@@ -0,0 +1,24 @@
+//translate()
+//Write a function translate() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+
+function translate( text ){
+	var translation = '';
+	var currentChar;
+
+	for ( var i=0; i<text.length; i++ ){
+
+		currentChar = text[i];
+
+		var vowels = "aeiou "; //Vowels and Space
+
+    if ( vowels.indexOf( currentChar) >= 0 ){
+			translation += text[i];
+		}
+		else {
+			translation += text[i] + 'o' + text[i];
+		}
+
+	}
+
+	return translation;
+}
