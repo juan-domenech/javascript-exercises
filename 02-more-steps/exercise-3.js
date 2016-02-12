feeeccb554539f39db8d4b47738c20666dca9c82
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-12 14:17:17 +0000

    02-2 with multiply

---
+++ 02-more-steps/exercise-3.js
@@ -0,0 +1,14 @@
+//reverse()
+// Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+
+function reverse( text ){
+
+	var reversedText = '';
+
+	for (i = 0; i < text.length; i++ ){
+
+		reversedText += text [ (text.length - 1) - i ];
+	}
+	return reversedText;
+
+}
