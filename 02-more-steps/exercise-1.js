5276e83c9bb612abfeaf01c8320d4f9e17a7a2f1
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-12 12:09:39 +0000

    javascript-exercises: 02-1 Code cleaned by JuanMa

--- 02-more-steps/exercise-1.js
+++ 02-more-steps/exercise-1.js
@@ -1,35 +1,38 @@
 //translate()
 //Write a function translate() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 
-function translate(arg){
-	var result="";
-	//console.log(arg.length);
-	for ( var i=0; i<arg.length; i++){
+function translate( text ){
+	var translation = '';
+	var currentChar;
 
-		switch(arg[i]){
-			case("a"):
-				result=result+arg[i];
+	for ( var i=0; i<text.length; i++ ){
+
+		currentChar = text[i];
+
+		switch( text[i] ){
+			case('a'):
+				translation += text[i];
 				break;
-			case("e"):
-				result=result+arg[i];
+			case('e'):
+				translation += text[i];
 				break;
-			case("i"):
-				result=result+arg[i];
+			case('i'):
+				translation += text[i];
 				break;
-			case("o"):
-				result=result+arg[i];
+			case('o'):
+				translation += text[i];
 				break;
-			case("u"):
-				result=result+arg[i];
+			case('u'):
+				translation += text[i];
 				break;
-			case(" "):
-				result=result+" ";
+			case(' '):
+				translation += ' ';
 				break;
 			default:
-				result=result+arg[i]+"o"+arg[i];
+				translation += text[i] + 'o' + text[i];
 		}
 
 	}
-	return result;
-
+	return translation;
 }
+
