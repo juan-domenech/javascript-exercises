f841dfa7c63eed4a4e80d518704bc9f867683d5b
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-13 12:28:34 +0000

    javascript-exercises: 2-7 v1

---
+++ 02-more-steps/exercise-7.js
@@ -0,0 +1,23 @@
+//charFreq()
+//Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
+
+function charFreq( string ){
+
+	var obj={};
+
+	for (i=0; i < string.length; i++ ){
+
+		if ( obj[ string[i] ] == undefined ){
+
+			obj[ string[i] ] = 1;
+
+		}else{
+
+		obj[ string[i] ] = obj[ string[i] ] + 1;
+
+		}
+
+	}
+	return obj;
+
+}
