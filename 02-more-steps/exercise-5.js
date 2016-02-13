bd273ba683f3b45b5f845cd281d06796c2cd20fe
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-02-13 10:27:03 +0000

    javascript-exercises: 2-5

---
+++ 02-more-steps/exercise-5.js
@@ -0,0 +1,22 @@
+//findLongestWord()
+//Write a function findLongestWord() that takes an input of words and returns the length of the longest one.
+
+function findLongestWord( input ){
+
+	var longest=0;
+	var value=0;
+
+		for ( i in input ){
+
+		value = input[i].length;
+
+		if ( value > longest ){
+
+			longest = value;
+
+		}
+
+}
+return longest;
+
+}
